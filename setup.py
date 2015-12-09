2665415c34297380d2eb9eb5c405375d723761f3
Vilnis Termanis <vilnis.termanis@iotic-labs.com>
2015-12-09 10:48:38 +0000

    Licensing preparation
    - Added licensing information (Apache v2.0)
    - Bumped version to 0.8

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-# Copyright (c) 2015 Iotic Labs Ltd. All rights reserved.
+# Copyright 2015 Iotic Labs Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 from distutils.core import setup, Extension
 
-VERSION = '0.7'
+VERSION = '0.8'
 # see lz4/NEWS
 LZ4_VERSION = 'r131'
 
@@ -10,14 +23,12 @@ setup(
     name='py-lz4framed',
     version=VERSION,
     description='LZ4Frame library for Python (via C bindings)',
-    author='Iotic Labs Ltd.',
+    author='Iotic Labs Ltd',
     author_email='info@iotic-labs.com',
     maintainer='Vilnis Termanis',
     maintainer_email='vilnis.termanis@iotic-labs.com',
-    # Change this if/when making public (this is a required field), also add license field
     url='https://github.com/Iotic-Labs/py-lz4framed',
-    # TODO - set once making public
-    # license='???',
+    license='Apache License 2.0',
     packages=['lz4framed'],
     ext_modules=[
         Extension('_lz4framed', [
@@ -47,6 +58,7 @@ setup(
     keywords=('lz4framed', 'lz4frame', 'lz4'),
     classifiers=(
         'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         # Add license classifier here if available in classifiers list
         'Programming Language :: C',
