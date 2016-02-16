58eff5f2483604b2ad0d2f467b46c334fe8c1fdd
Vilnis Termanis <vilnis.termanis@iotic-labs.com>
2016-02-16 18:41:32 +0000

    Licensing information update

--- lz4framed/__init__.py
+++ lz4framed/__init__.py
@@ -1,10 +1,10 @@
-# Copyright 2015 Iotic Labs Ltd
+# Copyright (c) 2016 Iotic Labs Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://github.com/Iotic-Labs/py-lz4framed/blob/master/LICENSE
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
